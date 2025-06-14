6d0c4d6870dd1899bd224bf69b3d5e0db2532234
SIXR VR <sixrvr@studivo.com>
2025-06-14 22:25:12 +0000

    deleted duplicates

--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    allowedDevOrigins: [
-      'https://6000-firebase-studio-1747722195027.cluster-3ch54x2epbcnetrm6ivbqqebjk.cloudworkstations.dev'
-    ],
-  }
-};
