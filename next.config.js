2e7906c55c4716b663172a94faac4f5b076d5f28
SIXR VR <sixrvr@studivo.com>
2025-06-14 22:00:43 +0000

    Server disconnected

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    allowedDevOrigins: [
+      'https://6000-firebase-studio-1747722195027.cluster-3ch54x2epbcnetrm6ivbqqebjk.cloudworkstations.dev'
+    ]
+  }
+};
+allowedDevOrigins: ['https://6000-firebase-studio-...cloudworkstations.dev'],
